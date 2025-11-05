b2095ada82f5f5b55fd06c098da55e64b4c79ae1
Nhlanhla Vumase <nhlanhlavumase242@gmail.com>
2025-11-05 09:12:26 +0200

    keep app

---
+++ app/src/main/java/com/example/kwiktaxi/models/TripCreateRequest.java
@@ -0,0 +1,29 @@
+package com.example.kwiktaxi.models;
+
+public class TripCreateRequest {
+    private int user_id;
+    private int rank_destination_id;
+
+    public TripCreateRequest(int user_id, int rank_destination_id) {
+        this.user_id = user_id;
+        this.rank_destination_id = rank_destination_id;
+    }
+
+    public int getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRankDestinationId() {
+        return rank_destination_id;
+    }
+
+    public void setRankDestinationId(int rank_destination_id) {
+        this.rank_destination_id = rank_destination_id;
+    }
+}
+
+
